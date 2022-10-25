f51885fc36cbb78c5b0e2693caae16e8b6657c72
dkainer <dkainer@outlook.com>
2022-10-25 14:35:20 -0400

    general improvements and packaging stuff

--- R/iRF.R
+++ R/iRF.R
@@ -1,18 +1,22 @@
 #' @title iterative Random Forest
-#' @description iterative Random Forest
+#' @name iRF
+#' @description an algorithmic advancement of Random Forest (RF),
+#' which takes advantage of Random Forests ability to determine feature importance,
+#' and produces a more accurate model by iteratively creating weighted forests.
 #'
-#' @param x feature matrix (i.e. the predictors for random forest)
-#' @param y the dependent variable (i.e. the variable being predicted)
-#' @param ntree number of trees in each random forest
-#' @param iter number of iterations to run
-#' @param classification boolean. If FALSE, then use Regression RF. Default = FALSE
-#' @param threads default = 1
-#' @param alwayssplits Character vector with variable names to be always selected in addition to the mtry variables tried for splitting.
-#' @param mtry Number of variables to possibly split at in each node.
+#' @param x Matrix. Features as columns (i.e. the predictors for random forest), samples as rows
+#' @param y Numeric. The dependent variable (i.e. the variable being predicted)
+#' @param iter Integer. number of iterations to run. Default = 5.
+#' @param mtry Numeric. Number of variables to possibly split at in each node.
 #' Default is the (rounded down) square root of the number variables with importance > 0.
 #' If 0 < mtry < 1, then it will be treated as a proportion of variables with importance > 0.
 #' Otherwise it is the absolute number of variables to use when splitting a node.
-#' @param saveall if TRUE then return the final random forest from each iteration as a list of forests
+#' @param classification Boolean. If FALSE, then use Regression RF. Default = FALSE
+#' @param saveall Boolean. if TRUE then return the final random forest from each iteration as a list of forests
+#' @param usepvals Boolean. Apply permutation testing to feature importances in order to cull features based on P-values
+#' @param verbose Boolean. default = TRUE
+#' @param ... further parameters for Ranger::ranger function
+#'
 #'
 #' @return One of two possible results:
 #' if saveall=TRUE, returns a list of random forest objects (one per iteration).
@@ -26,45 +30,74 @@
 #'     some features have their importance boosted while others go to zero and are culled from the forest.
 #'     The result is often a more accurate set of high ranked predictors from a better fit and simpler model.
 #'
+#'     If you set **usepvals=TRUE** then Ranger will use Altmann's method for calculating importance P-values using permutation.
+#'     iRF will then cull features based on having a conservative False Discovery Rate Q > 0.2 (i.e. it sets their importance to 0).
+#'     This usually results in a harsher culling of features than just allowing importances to naturally fall to zero.
+#'
+#'
 #' @references Basu S, Kumbier K, Brown JB et al. (2018) Iterative random forests to discover predictive and stable high-order interactions. Proc Natl Acad Sci USA 115, 1943–1948.
 #' @examples
 #'
 #' data(expdata)
-#' # use the first 99 genes expression to predict the expression of the 100th gene
-#' irf <- iRF(x = expdata[, 1:99], y = expdata[, 100], ntree = 100, threads=1, saveall=FALSE)
-#' # view top 10 most important genes
+#' ## use the first 99 genes' expression to predict the expression of the 100th gene
+#' irf <- iRF(x = expdata[, 1:99], y = expdata[, 100], saveall=FALSE, num.trees = 100)
+#' ## view top 10 most important genes
+#' ## Not run:
 #' sort(irf$variable.importance, decreasing = TRUE)[1:10]
 #'
-iRF <- function(x, y, ntree=500, iter=5, classification=F, threads=1, alwayssplits=NULL, mtry=NULL, saveall=TRUE)
+#' @seealso \link[ranger]{ranger} \link[iterativeRF]{iRF_LOOP}
+#'
+iRF <- function(x, y, iter=5, mtry=NULL, classification=FALSE, saveall=TRUE, usepvals = FALSE, verbose=TRUE, ...)
 {
   tmp <- cbind(x, Y = y)
   wt  <- rep(1/ncol(x), ncol(x)) # start with equal sample weighting per SNP
   rfs <- list()
   for(i in 1:iter)
   {
-    cat("\niRF iteration ",i,"\n")
-    cat("=================\n")
+
     if(is.null(mtry)){
       m = sqrt(sum(wt>0))
     } else if(mtry <= 1) {
       m = mtry*sum(wt>0)
     }
 
-    rf <- ranger::ranger(dependent.variable.name = "Y", data = tmp, num.trees=ntree,
-                         split.select.weights = wt, classification = classification,
-                         mtry = m, importance = "impurity", num.threads=threads, write.forest = TRUE,
-                         always.split.variables = alwayssplits)
+    rf <- ranger::ranger(dependent.variable.name = "Y",
+                         data = tmp,
+                         split.select.weights = wt,
+                         mtry = m,
+                         classification = classification,
+                         importance = "impurity_corrected",
+                         ...)
 
-    wt        <- rf$variable.importance / sum(abs(rf$variable.importance)) # scale importances to range(0,1)
-    wt[wt<0]  <- 0 # set negative weights to zero
+    # for the first iteration we used double as many trees and get importance p-values
+    if(usepvals == TRUE)
+    {
+      imp.pvals <- ranger::importance_pvalues(rf, method = "altmann", formula = Y ~ ., data = tmp, num.permutations = 500)
+      imp.pvals[ imp.pvals[,"importance"] < 0 , "importance" ] <- 0
+      fdr <- stats::p.adjust(p = imp.pvals[,"pvalue"], method = "fdr")
+      imp.pvals[ fdr > 0.2 , "importance" ] <- 0
+      wt <- imp.pvals[,"importance"] / sum(imp.pvals[,"importance"])
+
+    } else {
+      imp <- rf$variable.importance
+      wt  <- imp / sum(abs(imp)) # scale importances to range(0,1)
+      wt[wt<0]  <- 0 # set negative weights to zero
+    }
+
     rf$variable.importance <- wt
-    cat("mtry:  ", m, "\n")
-    cat("prediction error:  ",rf$prediction.error,"\n")
-    if(classification==FALSE) cat("R^2:   ",rf$r.squared,"\n")  # variance explained, computed on OOB data
-    if(classification==TRUE) print(rf$confusion.matrix)
 
-    cat("OOB cor(y,yhat):   ",stats::cor(rf$predictions,y),"\n")
-    cat("Features with importance > 0:",sum(wt>0),"\n")
+    if(verbose)
+    {
+      cat("\niRF iteration ",i,"\n")
+      cat("=================\n")
+      cat("mtry:  ", m, "\n")
+      cat("prediction error:  ",rf$prediction.error,"\n")
+      if(classification==FALSE) cat("R^2:   ",rf$r.squared,"\n")  # variance explained, computed on OOB data
+      if(classification==TRUE) print(rf$confusion.matrix)
+
+      cat("OOB cor(y,yhat):   ",stats::cor(rf$predictions,y),"\n")
+      cat("Features with importance > 0:",sum(wt>0),"\n")
+    }
 
     rfs[[i]] <- rf
 
@@ -82,4 +115,5 @@ iRF <- function(x, y, ntree=500, iter=5, classification=F, threads=1, alwaysspli
   }
 
 }
+
 
--- R/iRF_LOOP.R
+++ R/iRF_LOOP.R
@@ -1,11 +1,10 @@
-#' @title iRF_LOOP (Leave One Out Prediction)
+#' @title iRF_LOOP
+#' @name iRF_LOOP
 #' @description iRF_LOOP (Leave One Out Prediction)
 #'
 #' @param xmat feature matrix (i.e. the predictors for random forest)
 #' @param iter number of iterations to run in each iRF model
-#' @param ntree number of trees in each random forest. Default = 20.
 #' @param verbose default = FALSE
-#' @param threads Parallelize it. Do not set this higher than the number of cores you have. Default = 1.
 #'
 #' @return a data.frame containing the edges of the resulting network. This will have 4 columns:
 #' \describe{
@@ -14,38 +13,68 @@
 #'   \item{imp}{normalized importance score for that feature combo}
 #'   \item{R2}{the fit (variance explained) of the model that generated this edge}
 #' }
-#'
-#' @importFrom foreach %dopar%
+#' Only edges with importance > 0 are returned.
 #'
 #' @export
+#'
+#' @details Given a data set of n features and m samples, iRF Leave One Out Prediction (iRF-LOOP)
+#' starts by treating one feature as the dependent variable (Y) and the
+#' remaining n − 1 features as predictors (X matrix).
+#' Using an iRF model, the importance of each feature in X, for predicting Y, is calculated.
+#' The result is a vector, of size n, of importance scores (the importance score of Y,
+#' for predicting itself is set to zero). This process is repeated for each of the n features,
+#' requiring n iRF runs which produces n vectors of importance scores.
+#' To keep importance scores on the same scale across the n runs, each run's result is
+#' normalized relative to the sum of that run's importance score vector. When all n runs are combined
+#' it represents a directed network where features are the nodes, and the ability of one feature to predict another
+#' (conditional on all other features) is represented as edges between nodes with weights defined by normalized importance scores.
+#'
+#'     iRF-LOOP does the same overall process as **GENIE3**, but replaces the standard RF model with iRF.
+#'     Walker et al (2022) demonstrated that replacing RF with iRF produces smaller and less noisy networks when
+#'     applied to gene expression data. True positive edges usually rank higher in the edge list (ranked by importance score)
+#'     when using iRF-LOOP rather than GENIE3.
+#'
+#'     iRF-LOOP can be used on any type of input data as long as it is in matrix form. However, for very large
+#'     datasets where there may be tens of thousands of predictors (e.g. whole transcriptome expression data in hundreds of samples),
+#'     this R package is likely to take considerable run time due to the need to run 10s of thousands of iRF models. For these
+#'     situations we direct the user to the massively parallel [HPC implementation]<https://github.com/Jromero1208/RangerBasediRF>
 #'
 #' @references Cliff, Ashley, et al. "A high-performance computing implementation of iterative random forest for the creation of predictive expression networks." Genes 10.12 (2019): 996.
 #' @references Walker AM, Cliff A, Romero J, Shah MB, Jones P, Gazolla JG, Jacobson DA, Kainer D. Evaluating the performance of random forest and iterative random forest based methods when applied to gene expression data. Computational and Structural Biotechnology Journal. 2022 Jan 1;20:3372-86.
 #' @examples
 #' data(expdata)
-#' # make a Predictive Expression Network
-#' pen <- iRF_LOOP(expdata, ntree=100)
+#' ## make a Predictive Expression Network
+#' pen <- iRF_LOOP(expdata, num.trees=100)
 #'
-#' # view top 10 edges in resulting PEN
+#' ## view top 10 edges in resulting PEN
+#' ## Not run:
 #' top <- pen[order(pen$imp, decreasing = TRUE),][1:10,]
-#' # note that the some top edges may come from a very poor model (low R2)
+#' ## note that the some top edges may come from a very poor model (low R2)
 #'
+#' @seealso \link[iterativeRF]{iRF}
 globalVariables("g") # we need this to make foreach work with CRAN checks
-iRF_LOOP <- function(xmat, iter=3, ntree=100, verbose=FALSE, threads=1)
+iRF_LOOP <- function(xmat, iter=3, verbose=FALSE,  ...)
 {
   #for each feature in the X matrix, use it as a Y vector in iRF
-  cl = parallel::makeCluster(threads)
-  doParallel::registerDoParallel(cl)
-  out <- foreach::foreach(g = 1:ncol(xmat), .combine=rbind, .export = "iRF") %dopar%
+  # out <- foreach::foreach(g = 1:ncol(xmat), .export = "iRF") %do%
+  # {
+  #     y   <- xmat[,g]
+  #     x   <- xmat[,-g]
+  #     irf <- iRF(x = x, y = y, iter=iter, mtry = NULL, saveall = FALSE, verbose=verbose, ...)  # must have saveall=F
+  #     df  <- data.frame(featX = colnames(xmat)[-g], featY = colnames(xmat)[g], imp=irf$variable.importance, R2=irf$r.squared, row.names = NULL)
+  #     df[df$imp>0,]
+  # }
+
+  out <- list()
+  for(g in 1:ncol(xmat))
     {
-      #source("~/devwork/_SCRIPTS/iRF.R")
       y   <- xmat[,g]
       x   <- xmat[,-g]
-      irf <- iRF(x = x, y = y, ntree = ntree, threads = 1, iter=iter,  mtry = NULL, saveall = FALSE)  # must have saveall=F
-
+      irf <- iRF(x = x, y = y, iter=iter, mtry = NULL, saveall = FALSE, verbose=verbose, ...)  # must have saveall=F
       df  <- data.frame(featX = colnames(xmat)[-g], featY = colnames(xmat)[g], imp=irf$variable.importance, R2=irf$r.squared, row.names = NULL)
-      df[df$imp>0,]
+      out[[g]] <- df[df$imp>0,]
     }
-  parallel::stopCluster(cl)
+
+  out <- do.call('rbind', out)
   return(out)
 }
